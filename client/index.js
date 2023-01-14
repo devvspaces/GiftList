77f3e6472758a96717ee58f7e282cc0121d2d7eb
devvspaces <netrobepy@gmail.com>
2023-01-14 10:05:51 +0100

    Completed the Gift List project

--- client/index.js
+++ client/index.js
@@ -1,14 +1,34 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
+// Using yargs to parse out cmd arguments
+const args = yargs(hideBin(process.argv))
+    .option('name', {
+        alias: 'n',
+        type: 'string',
+        description: 'Name to check if on gift list',
+        demandOption: true
+    })
+    .parse()
+
+
+const name = args.name
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // Generate a merkle tree with the nice list 
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name)
+
+  // Generate a merkle proof to send to the server
+  const proof = merkleTree.getProof(index)
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
